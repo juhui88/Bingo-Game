154b6f4d9f31a6204a1bd879c0835f9bcae414f4
unknown <sand8594@hufs.ac.kr>
2020-10-02 15:10:48 +0900

    20201002

---
+++ bingo game 1.cpp
@@ -0,0 +1,61 @@
+﻿#include <iostream>
+#include <time.h>
+using namespace std;
+
+int main() {
+
+	srand((unsigned int)time(0));
+
+	int iNumber[25] = {};
+
+	for (int i = 0; i < 25; ++i) {
+		iNumber[i] = i + 1;
+	}
+	int iTemp, idx1, idx2;
+	for (int i = 0; i < 100; ++i) {
+		idx1 = rand() % 25;
+		idx2 = rand() % 25;
+
+		iTemp = iNumber[idx1];
+		iNumber[idx1] = iNumber[idx2];
+		iNumber[idx2] = iTemp;
+	}
+
+	while (true) {
+		system("cls");
+
+		for (int i = 0; i < 5; ++i) {
+			for (int j = 0; j < 5; ++j) {
+				if (iNumber[i * 5 + j] == INT_MAX)
+					cout << "*\t";
+				else
+					cout << iNumber[i * 5 + j] << "\t";
+			}
+			cout << endl;
+
+		}
+		cout << "숫자를 이력하세요(0 : 종료) : ";
+		int iInput;
+		cin >> iInput;
+
+		if (iInput == 0)
+			break;
+		else if (iInput < 1 || iInput > 25)
+			continue;
+
+		bool bAcc = true;
+
+		for (int i = 0; i < 25; ++i) {
+			if (iInput == iNumber[i]) {
+				bAcc = false;
+
+				iNumber[i] = INT_MAX;
+
+				break;
+			}
+		}
+		if (bAcc)
+			continue;
+	}
+	return 0;
+}
